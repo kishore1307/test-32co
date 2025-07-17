abc85b835ae56fbfc82025012593e04d78f6913f
kishore1307 <67825033+kishore1307@users.noreply.github.com>
2025-07-18 00:09:44 +0530

    Create alarms.tf

---
+++ observability/alarms.tf
@@ -0,0 +1,18 @@
+
+---
+```hcl
+resource "aws_cloudwatch_metric_alarm" "cpu_alarm" {
+  alarm_name          = "HighCPUUtilization"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "This metric monitors high CPU usage"
+  alarm_actions       = []
+  dimensions = {
+    InstanceId = aws_instance.app.id
+  }
+}
