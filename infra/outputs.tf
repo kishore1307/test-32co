6cb3ac0c9533e56762649509a59f97ffc64af762
kishore1307 <67825033+kishore1307@users.noreply.github.com>
2025-07-17 23:33:24 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,19 +1,3 @@
-output "alb_dns" {
-  value = aws_lb.app_alb.dns_name
-}
-
-output "db_endpoint" {
-  value = aws_db_instance.app_db.address
-}
-
-output "s3_bucket" {
-  value = aws_s3_bucket.static_assets.bucket
-}
-
-output "cw_alarm_arn" {
-  value = aws_cloudwatch_metric_alarm.high_cpu.arn
-}
-
-output "acm_certificate_arn" {
-  value = var.enable_https ? aws_acm_certificate.ssl_cert[0].arn : null
+output "instance_public_ip" {
+  value = aws_instance.app.public_ip
 }
